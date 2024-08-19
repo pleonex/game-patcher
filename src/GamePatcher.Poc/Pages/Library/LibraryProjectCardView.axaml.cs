84a63ac05e264522871282479bd5fea1a08a3f58
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-08-20 00:30:43 +0200

    :sparkles: Implement PoC library card

---
+++ src/GamePatcher.Poc/Pages/Library/LibraryProjectCardView.axaml.cs
@@ -0,0 +1,20 @@
+﻿namespace PleOps.GamePatcher.Poc.Pages.Library;
+
+using Avalonia.Controls;
+using PleOps.GamePatcher.Poc.Pages;
+
+public partial class LibraryProjectCardView : UserControl
+{
+    public LibraryProjectCardView()
+    {
+        InitializeComponent();
+    }
+}
+
+internal class LibraryProjectCardViewModelDesigner : LibraryProjectCardViewModel
+{
+    public LibraryProjectCardViewModelDesigner()
+        : base(DesignerDataProvider.GetExampleModdingProjectManifest())
+    {
+    }
+}
---
+++ src/GamePatcher.Poc/Pages/Library/LibraryProjectCardViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace PleOps.GamePatcher.Poc.Pages.Library;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PleOps.GamePatcher.Poc.ModdingProject;
+
+public partial class LibraryProjectCardViewModel : ViewModelBase
+{
+    public LibraryProjectCardViewModel(ModdingProjectManifest manifest)
+    {
+        ArgumentNullException.ThrowIfNull(manifest);
+
+        ProjectInfo = manifest.Project;
+        Platforms = manifest.Products.Select(p => p.Platform).Distinct();
+        Languages = manifest.Mods.Select(m => m.TargetLanguage).Distinct();
+    }
+
+    public Project ProjectInfo { get; }
+
+    public IEnumerable<string> Platforms { get; }
+
+    public IEnumerable<string> Languages { get; }
+}
