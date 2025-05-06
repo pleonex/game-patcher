69e68f742daaf5437d16489866272524329e0683
Benito Palacios Sanchez <pleonex@protonmail.com>
2025-05-06 19:38:40 +0200

    :sparkles: Implement DS game ID verification

---
+++ src/Moxmi.Platforms.Ekona/Compatibility/DsGameIdCompatibilityValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace PleOps.Moxmi.Platforms.Ekona.Compatibility;
+
+using System.Threading.Tasks;
+using PleOps.Moxmi.Compatibility;
+using SceneGate.Ekona.Containers.Rom;
+using Yarhl.IO;
+
+public class DsGameIdCompatibilityValidator : ICompatibilityValidator
+{
+    public Task<bool> VerifyCompatibilityAsync(string softwarePath, string compatibilityValue)
+    {
+        // We only need the header, no need to parse the full ROM
+        using var softwareData = new BinaryFormat(softwarePath, FileOpenMode.Read);
+        RomHeader header = new Binary2RomHeader().Convert(softwareData);
+
+        ProgramInfo info = header.ProgramInfo;
+        string gameId = $"{info.GameCode}-{info.MakerCode}-{info.Version:D2}";
+
+        return Task.FromResult(gameId == compatibilityValue);
+    }
+}
--- src/Moxmi.Platforms.Ekona/EkonaPlatformProviderExtensions.cs
+++ src/Moxmi.Platforms.Ekona/EkonaPlatformProviderExtensions.cs
@@ -1,12 +1,15 @@
 ﻿namespace PleOps.Moxmi.Platforms.Ekona;
 
 using PleOps.Moxmi.ModInstaller;
+using PleOps.Moxmi.Platforms.Ekona.Compatibility;
 using PleOps.Moxmi.Platforms.Ekona.Integrity;
 
 public static class EkonaPlatformProviderExtensions
 {
     public static ModInstallerWorkflowProvider RegisterEkona(this ModInstallerWorkflowProvider provider)
     {
+        provider.RegisterCompatibilityValidator("ds-gameid", new DsGameIdCompatibilityValidator());
+
         // TODO: register keys
         provider.RegisterIntegrityValidator("ds-rom", new DSiRomIntegrityValidator(null));
         provider.RegisterIntegrityValidator("dsi-rom", new DSiRomIntegrityValidator(null));
