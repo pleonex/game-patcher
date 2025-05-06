7272aa48b603dba26e0419111745817851b2bf11
Benito Palacios Sanchez <pleonex@protonmail.com>
2025-05-06 19:40:32 +0200

    :sparkles: PoC of installer via console app

--- src/Moxmi.Console/Installer/InteractiveInstallerCommand.cs
+++ src/Moxmi.Console/Installer/InteractiveInstallerCommand.cs
@@ -3,22 +3,73 @@
 using System.ComponentModel;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using PleOps.Moxmi.ModInstaller;
+using PleOps.Moxmi.Platforms.Ekona;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using Spectre.Console.Extensions;
 
 [Description("Install a modding project or specific mod with user inputs")]
 internal class InteractiveInstallerCommand : AsyncCommand<InteractiveInstallerCommand.Settings>
 {
     private ILogger<InteractiveInstallerCommand> logger = null!;
 
-    public override Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
         AppLoggerFactory.MinimumLevel = settings.Verbosity;
         logger = AppLoggerFactory.CreateLogger<InteractiveInstallerCommand>();
-
         logger.LogInformation("Starting to apply mod!");
 
-        return Task.FromResult(0);
+        var workflowProvider = new ModInstallerWorkflowProvider();
+        workflowProvider.RegisterEkona();
+
+        AnsiConsole.Write(new Rule("Mod installer"));
+        var deserializer = new ModInstallerExtensibleSerializer();
+        using var mixData = File.OpenRead(settings.ModPath);
+        var mix = deserializer.Deserialize(mixData);
+        AnsiConsole.MarkupLineInterpolated($"[bold blue]{mix.Mod.Name} v{mix.Mod.Version}[/]");
+        AnsiConsole.MarkupLineInterpolated($"[italic gray]by {mix.Mod.Authors}[/]");
+        AnsiConsole.MarkupLineInterpolated($"{mix.Mod.Description}");
+
+        AnsiConsole.Write(new Rule("Compatibility verification"));
+        CompatibleProductInfo? product = null;
+        foreach (var compatibleProduct in mix.Mod.Compatibility) {
+            AnsiConsole.MarkupLineInterpolated($"Checking match with {compatibleProduct.Name}");
+
+            var verificationInfo = compatibleProduct.Verification;
+            try {
+                var validator = workflowProvider.GetCompatibilityValidator(verificationInfo.Method);
+
+                var isCompatible = await validator.VerifyCompatibilityAsync(settings.SoftwarePath, verificationInfo.Hash);
+                if (isCompatible) {
+                    AnsiConsole.MarkupLineInterpolated($"[green]Match![/] Product ID: {compatibleProduct.ProductId}");
+                    product = compatibleProduct;
+                    break;
+                }
+            }
+            catch (Exception) {
+                AnsiConsole.MarkupLineInterpolated($"[red]Cannot validate with method {verificationInfo.Method}[/]");
+            }
+        }
+
+        if (product is null) {
+            AnsiConsole.MarkupLine("[red]No compatible product found[/]");
+            return 1;
+        }
+
+        AnsiConsole.Write(new Rule("Integrity verification"));
+        var integrity = workflowProvider.GetIntegrityValidator(product.Format);
+        var status = await integrity.VerifyIntegrityAsync(settings.SoftwarePath);
+        AnsiConsole.MarkupLine($"Is data valid: {status.IsDataValid}");
+        AnsiConsole.MarkupLine($"Signed: {status.HasValidPublisherSignature}");
+
+        AnsiConsole.Write(new Rule("Installation"));
+        AnsiConsole.MarkupLine("[gray]Applying resources...[/]");
+
+        AnsiConsole.Write(new Rule("Bundle"));
+        AnsiConsole.MarkupLine("[gray]Creating new software bundle...[/]");
+
+        return 0;
     }
 
     public sealed class Settings : CommandSettings
@@ -43,8 +94,8 @@ internal class InteractiveInstallerCommand : AsyncCommand<InteractiveInstallerCo
 
         public override ValidationResult Validate()
         {
-            if (!File.Exists(SoftwarePath)) {
-                return ValidationResult.Error($"The input software file '{SoftwarePath}' does NOT exists");
+            if (!File.Exists(SoftwarePath) && !Directory.Exists(SoftwarePath)) {
+                return ValidationResult.Error($"The software path '{SoftwarePath}' does NOT exists");
             }
 
             if (!File.Exists(ModPath)) {
